7ddea322e400c93cf0cdaa06ecfabd62bb3e044e
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-10-28 08:00:01 -0300

    feat: backend gobarber NEEDS CODE CHECK

--- primeiro-projeto-node/src/routes.ts
+++
@@ -1,5 +0,0 @@
-import {Request, Response} from 'express';
-
-export function blabla(request : Request, response : Response){
-  return response.json({message: "Hello GoStack!"})
-}
--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -3,9 +3,10 @@ import routes from './routes/';
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
-app.use(express.json())
 
-app.listen(3000, () => {
-  console.log("🚀 Server started on port 3000")
+
+app.listen(3333, () => {
+  console.log("🚀 Server started on port 3333")
 })
