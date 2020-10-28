e5d937a72472368040f910d9f8e3b9a85783a81e
Herllon Cardoso <herllon.cardoso@alfalaval.com>
2020-10-28 16:15:48 -0300

    add code comment about design patters

--- primeiro-projeto-node/src/repositories/AppointmentsRepository.ts
+++ primeiro-projeto-node/src/repositories/AppointmentsRepository.ts
@@ -2,33 +2,47 @@ import Appointment from '../models/Appointment';
 import { isEqual } from 'date-fns';
 //data transfer object
 interface CreateAppointmentDTO {
+  //temos uma interface que fará uma transferencia de dados
+  //declaremos o formato das nossos objetos.
   provider: string;
   date: Date;
 }
 
+//criar uma classe que armazenara o repositorio de agendamentos
 class AppointmentsRepository {
+  // uma variavel privada usara o model Appointment.
   private appointments: Appointment[];
 
   constructor() {
+    //construira nosso agendamento.
     this.appointments = [];
   }
-
+//metodo publico que retornara ALL elementos dentro do agendamentos
   public all(): Appointment[] {
     return this.appointments;
   }
 
+  //metodo publico que vai procurar agendamentos por data, recebendo a data por parametro
+  //dentro da variavel findAppointment ele irá procurar dentro dos agendamentos, por um
+  //agendamento que seja IGUAL a data.
   public findByDate(date: Date): Appointment | null {
     const findAppointment = this.appointments.find(appointment =>
       isEqual(date, appointment.date),
     );
-
+    //se tiver encontrado vai retorna-lo, se nao encontrar. será NULL.
     return findAppointment || null;
   }
 
+  //metodo publico create para criar um agendamento no sistema.
+  //metodo create vai receber o provider e o data que sera no formato da interface
+  //DTO criada acima
+  //que tambem sera nop formato do nosso appointment que se encontra no nosso model
   public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    //variavel appointment que recebera um novo appointment recebendo o provider e date.
     const appointment = new Appointment({ provider, date });
+    //adicionara no agendamentos o agendamento.
     this.appointments.push(appointment);
-
+    //retornara o agendamento
     return appointment;
   }
 }
--- primeiro-projeto-node/src/routes/appointments.routes.ts
+++ primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -1,36 +1,60 @@
-import { response, Router } from 'express';
+//tendo toda nossas rotas de agendamento em um arquivo só, temos que dividir as responsabilidades.
+//entao dividimos em repositories e models.
+
+
+import { Router } from 'express';
+//Biblioteca date-fns que é otima para trabalharmos com datahora,
+//nesse caso importamos o startOfHour, que sempre arredondara os minutos pro inicio da hora que se encontra
+//e o parseISO que ira converter a data no formato ISO.
 import { startOfHour, parseISO } from 'date-fns';
+//importar o repositorio(padrao de projeto)
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+//dentro do nosso agendamento usaremos o Router do express para criar todas nossas rotas necessarias
+//para o agendamento
 
 const appointmentsRouter = Router();
+
+//variavel appointmentRepository ira receber um novo AppointmentsRepository
 const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.get('/', (request, response) => {
+  //na constante appointments iremos receber o metodo all do appointmentsRepository
   const appointments = appointmentsRepository.all();
 
+  //retornara todos os appointments
   return response.json(appointments);
 });
 
+//rota para criar um agendamento
 appointmentsRouter.post('/', (request, response) => {
+  //iremos receber provider e date inseridos pelo usuario em sua requisição pelo corpo.
   const { provider, date } = request.body;
 
+  // parsedDate vai receber a data convertida na iso e no seu inicio da hora. usando os metodos
+  // startOfHour e parseISO que vem da lib date-fns
   const parsedDate = startOfHour(parseISO(date));
 
+  //procurar agendamentos na memsa data vai receber de dentro dos repositorios de agendamento o metodo
+  //procurar por datas.
   const findAppointmentInSameDate = appointmentsRepository.findByDate(
     parsedDate,
   );
 
+  //se retornar true(por usamos o .FIND porque ele retorna true e false)
+  //vai receber a mensagem que ja foi agendada.
   if (findAppointmentInSameDate) {
     return response
       .status(400)
       .json({ message: 'This appointment is already booked' });
   }
-
+//se retornar false, ele ira criar uma variavel appointment que receberar
+// do nosso repositorio o metodo .create, passando o provider e o date(convertido)
   const appointment = appointmentsRepository.create({
     provider,
     date: parsedDate,
   });
 
+  //retornando a resposta com o agendamento feito.
   return response.json(appointment);
 });
 
