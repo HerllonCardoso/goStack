5d5c46c597a6e63a91fb9838454e6566f8028314
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-11-03 20:31:23 -0300

    feat: saving appointments to database

--- primeiro-projeto-node/src/database/migrations/1604436450087-CreateAppointments.ts
+++ primeiro-projeto-node/src/database/migrations/1604436450087-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1604436450087
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
--- primeiro-projeto-node/src/models/Appointment.ts
+++ primeiro-projeto-node/src/models/Appointment.ts
@@ -1,21 +1,14 @@
 import { uuid } from 'uuidv4';
-//Aqui criamos uma classe que será o modelo do nosso dado.
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('appointments')
 class Appointment {
-  //passando o modelo dos dados.
+  @PrimaryGeneratedColumn('uuid')
   id: string;
-
+  @Column()
   provider: string;
-
+  @Column('timestamp with time zone')
   date: Date;
-
-  //criar um construtor que construira nosso dados, o Omit é para simplismente ele ignorar o ID na
-  //passagem de dados por parametros, pois o Id pegaremos do nossa lib uuuid, e provider e date virá
-  //no corpo da requisição feita pelo usuário
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
 }
 
 export default Appointment;
