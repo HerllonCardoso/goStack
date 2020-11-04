7f9c41acebd15fa0f71077bbef3e999c5a9cfb95
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-11-04 08:02:50 -0300

    feat: create users, check delete ts error

--- primeiro-projeto-node/src/routes/index.ts
+++ primeiro-projeto-node/src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'; //importamos o modulo Router do express.
 import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
 
 //criamos uma variavel routes recebendo o uso da função Routes do express.
 const routes = Router();
@@ -7,5 +8,6 @@ const routes = Router();
 //agora podemos criar uma rota usando o routes.use, o caminho dela, e o nome da rota(n esquecer
 //de importar.)
 routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
