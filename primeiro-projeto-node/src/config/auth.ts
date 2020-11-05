100db77447a88bdce8f7c566704dfa2537200ff7
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-11-05 20:34:47 -0300

    feat: authentication w/ jwt

---
+++ primeiro-projeto-node/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'c0b84f3ff6086f595261ce38fed468e6',
+    expiresIn: '1d',
+  },
+};
--- primeiro-projeto-node/src/routes/index.ts
+++ primeiro-projeto-node/src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'; //importamos o modulo Router do express.
 import appointmentsRouter from './appointments.routes';
 import usersRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
 
 //criamos uma variavel routes recebendo o uso da função Routes do express.
 const routes = Router();
@@ -9,5 +10,6 @@ const routes = Router();
 //de importar.)
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
