1c27fd5a687adb00f142f171fe224506f0ed3bad
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-11-08 13:51:52 -0300

    feat: exception handling

--- primeiro-projeto-node/src/routes/index.ts
+++ primeiro-projeto-node/src/routes/index.ts
@@ -1,13 +1,10 @@
-import { Router } from 'express'; //importamos o modulo Router do express.
+import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
 import usersRouter from './users.routes';
 import sessionsRouter from './sessions.routes';
 
-//criamos uma variavel routes recebendo o uso da função Routes do express.
 const routes = Router();
 
-//agora podemos criar uma rota usando o routes.use, o caminho dela, e o nome da rota(n esquecer
-//de importar.)
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
